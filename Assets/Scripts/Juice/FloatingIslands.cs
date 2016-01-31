ddd2088e2ad32452f5d8347f598e368ea0632257
Sean Oxspring <Sean.Oxspring@hotmail.com>
2016-01-31 09:25:03 +0000

    Clouds and Floating Blocks
    Clouds now move, so do the tiny island pieces around the main game area.

---
+++ Assets/Scripts/Juice/FloatingIslands.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingIslands : MonoBehaviour {
+
+    private Vector3 actualPosition;
+    public float floatAmount;
+    private float circleTimer;
+    private float randomSpeed;
+
+	// Use this for initialization
+	void Start () {
+        actualPosition = this.transform.position;
+        randomSpeed = Random.Range(0.75f, 2.0f);
+        circleTimer = Random.Range(0, 2 * Mathf.PI);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        circleTimer += Time.deltaTime / randomSpeed;
+        if(circleTimer > 2 *Mathf.PI)
+        {
+            circleTimer -= 2 * Mathf.PI;
+        }
+        float floatLocation = actualPosition.y + (Mathf.Sin(circleTimer) * floatAmount);
+        this.transform.position = new Vector3(this.transform.position.x,
+                                                floatLocation,
+                                                this.transform.position.z);
+	}
+}
