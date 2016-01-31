f6864906b4f12e21a12d5417bc1da2e8f1c02f39
Sean Oxspring <Sean.Oxspring@hotmail.com>
2016-01-31 12:44:19 +0000

    Added more juiciness!
    Moved a bunch of the islands around from underneath the island, they
    weren't being seen by the camera which was a real shame. Also added some
    animated fire underneath the pot.

---
+++ Assets/Scripts/Juice/FireShakeRattleAndRoll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireShakeRattleAndRoll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Random.Range(1,10) < 6)
+        this.transform.localScale = new Vector3(Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f), Random.Range(0.5f, 1.0f));
+    }
+}
